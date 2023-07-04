5f6c697be4cd1a6ee48f2f392d7c0be1b8023018
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-04 18:08:16 -0300

    Criando a pageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from "react"
+import PageHeader from "../template/pageHeader"
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história:</h2>
+        <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio dolore nesciunt, aperiam molestias, nulla sit facere cumque voluptatibus maxime sunt, nisi officia iusto assumenda. Alias nisi amet sit ex a.
+            Lorem ipsum dolor sit amet consectetur adipisicing esse mollitia ut porro consectetur dignissimos?</p>
+        <h2>Missão e Visão:</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipis Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas magnam, placeat accusamus vitae velit aperiam possimus veritatis doloribus officia quibusdam tenetur sint cum nam cupiditate quae explicabo et. Impedit, sint?</p>
+        <h2>Imprensa:</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam possimus tenetur ab, error sit, culpa eaque ipsum itaque ratione in eius eum ea ad ipsam debitis! Fugiat laboriosam eum libero?</p>
+        <h3>Contatos:</h3>
+        <p>osam dicta, officiis libero.</p>
     </div>
 )
