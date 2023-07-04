3e637a1a730352bd7661bb6ce02c9eb56f825b96
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-04 19:04:00 -0300

    evento adicionar

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -10,7 +10,10 @@ export default props => (
        
        
        <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}>
+
+            </IconButton>
        </Grid>           
        
     </div>
