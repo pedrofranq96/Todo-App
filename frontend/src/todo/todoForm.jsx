7b47a82d49074d1d020b26f4620475201bb993f3
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-06 01:59:20 -0300

    Conectando o TodoForm ao redux

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
@@ -9,7 +10,7 @@ import IconButton from "../template/iconButton";
 // esc : limpa o campo de busca e retorna para todas as tarefas 
 //-----------------------------------------------------------------
 
-export default props => {
+const TodoForm = props => {
     const keyHandler = (e) =>{
         if(e.key === 'Enter'){
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -46,3 +47,6 @@ export default props => {
         )
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+export default connect(mapStateToProps)(TodoForm)
