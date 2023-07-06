3894f65711614760132ddc025ed4a29d7e50e87f
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-06 16:54:16 -0300

    aplicanto middleware(redux-promise

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-    description: 'ler livro',
-    list: [{
-      _id: 1,
-      description: 'pagar cartão',
-      done:true
-    },{
-      _id: 2,
-      description: 'pagar conta de luz',
-      done:false
-    },{
-      _id: 3,
-      description: 'consulta médica',
-      done:false
-    }]
-}
+const INITIAL_STATE = {description: '', list: []}
 
 export default (state = INITIAL_STATE, action) =>{
     switch(action.type){
