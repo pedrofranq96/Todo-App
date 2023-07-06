a76482220555766cab78be0966d3e7f9cc7230f1
pedrofranq96 <pedrofranq96@gmail.com>
2023-07-06 17:11:52 -0300

    aplicando middleware(redux-multi)

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import { applyMiddleware, createStore } from "redux"
 import { Provider } from "react-redux"
 import promise from "redux-promise"
+import multi from "redux-multi"
 
 
 import App from "./main/app"
@@ -10,8 +11,9 @@ import reducers from "./main/reducers"
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && 
     window.__REDUX_DEVTOOLS_EXTENSION__()
+    
 const store = 
-    applyMiddleware(promise)
+    applyMiddleware(multi,promise)
     (createStore)
     (reducers, devTools)
 ReactDOM.render(
